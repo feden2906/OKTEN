ea25d4a3db45eb9bd76ed880d5bc1fb1f2403f38
Freefireman <beluh.c.s@gmail.com>
2021-05-25 16:57:42 +0300

    it is your hw

--- REACT/react_les_3/src/components/post/post.js
+++
@@ -1,8 +0,0 @@
-export default function Post({item, findIdFunc}) {
-  return (
-    <div className={'post-box'}>
-        <p>{item.body}</p>
-        <button onClick={() => findIdFunc(item.id)}>details</button>
-    </div>
-  );
-}
