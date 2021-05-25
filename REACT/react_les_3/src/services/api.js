3263facf7bac9d5991e0853d28591e36b069060a
Freefireman <beluh.c.s@gmail.com>
2021-05-25 18:21:07 +0300

    it is your hw

--- REACT/react_les_3/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com',
-});
-
-axios.get('/users');
-axios.get('/posts');
--- REACT/react_les_4/src/components/character/character.component.js
+++
@@ -1,7 +0,0 @@
-export default function Character({person}) {
-  return (
-    <div>
-        <h3>{person.name}</h3>
-    </div>
-  );
-}
