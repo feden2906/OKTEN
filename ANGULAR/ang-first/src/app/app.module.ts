200f52fa14ba14a8948f1113f2fee709d1c19191
Melnyk Andriy <a_melnica@yahoo.com>
2021-05-31 15:52:29 +0300

    add some style

---
+++ ANGULAR/ang-first/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    // UserComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ANGULAR/ang-first/src/app/components/users/users.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IUser} from "../models/user";
+import  {UserService} from "../services/user.service"
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  // private httpClient: HttpClient;
+  public users: IUser[] = [];
+
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+this.userService.getUsers().subscribe(value => {
+  this.users = value
+});
+  }
+
+}
